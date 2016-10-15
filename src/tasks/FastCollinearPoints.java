d66c89d30011b8ff585023e8a34746d02ddc8046
Milos Stojanovic <milos92s@yahoo.com>
2016-10-16 00:04:05 +0200

    Fixed fast method bug

--- src/tasks/FastCollinearPoints.java
+++ src/tasks/FastCollinearPoints.java
@@ -6,7 +6,6 @@
 package tasks;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import static java.util.Arrays.sort;
 import java.util.Collections;
 import java.util.List;
@@ -25,49 +24,39 @@ public class FastCollinearPoints {
         }
 
         List<LineSegment> segmentsList = new ArrayList<>();
-
-        for (int p = 0; p < points.length; p++) {
-
+        for (int p = 0; p < points.length - 1; p++) {
             if (points[p] == null) {
                 throw new NullPointerException();
             }
 
             List<Point> slopedPoints = new ArrayList<>();
-            Point[] temp = Arrays.copyOf(points, points.length);
-//            Point[] temp = Arrays.copyOfRange(points, p, points.length);
-            sort(temp, points[p].slopeOrder());
-            Point root = temp[0];
-            double slope = root.slopeTo(temp[1]);
-            slopedPoints.add(temp[1]);
-            for (int i = 2; i < temp.length; i++) {
-                if (root.slopeTo(temp[i]) == slope) {
+            sort(points, p + 1, points.length, points[p].slopeOrder());
+            Point root = points[p];
+            double slope = root.slopeTo(points[p + 1]);
+            slopedPoints.add(points[p + 1]);
+            for (int i = p + 2; i < points.length; i++) {
+                if (root.slopeTo(points[i]) == slope) {
                     if (slopedPoints.isEmpty()) {
-                        slopedPoints.add(temp[i - 1]);
+                        slopedPoints.add(points[i - 1]);
                     }
-                    slopedPoints.add(temp[i]);
+                    slopedPoints.add(points[i]);
                 } else {
                     if (slopedPoints.size() >= 3) {
-                        Collections.sort(slopedPoints, root.slopeOrder());
-                        addLineSegment(segmentsList, new LineSegment(root, slopedPoints.get(slopedPoints.size() - 1)));
-                        slopedPoints = new ArrayList<>();
-                    } else {
-                        slopedPoints = new ArrayList<>();
+                        slopedPoints.add(root);
+                        Collections.sort(slopedPoints);
+                        addLineSegment(segmentsList, new LineSegment(slopedPoints.get(0), slopedPoints.get(slopedPoints.size() - 1)));
                     }
+                    slopedPoints = new ArrayList<>();
                 }
-                slope = root.slopeTo(temp[i]);
+                slope = root.slopeTo(points[i]);
             }
-
             if (slopedPoints.size() >= 3) {
-                Collections.sort(slopedPoints, root.slopeOrder());
-                addLineSegment(segmentsList, new LineSegment(root, slopedPoints.get(slopedPoints.size() - 1)));
-                slopedPoints = new ArrayList<>();
-            } else {
-                slopedPoints = new ArrayList<>();
+                slopedPoints.add(root);
+                Collections.sort(slopedPoints);
+                addLineSegment(segmentsList, new LineSegment(slopedPoints.get(0), slopedPoints.get(slopedPoints.size() - 1)));
             }
         }
-
         segments = segmentsList.toArray(new LineSegment[segmentsList.size()]);
-
     }
 
     public int numberOfSegments() {
@@ -88,5 +77,13 @@ public class FastCollinearPoints {
             System.out.print("(" + point.slopeTo(p) + ") " + p.toString() + "; ");
         }
         System.out.println("\n");
+    }
+
+    private void displayList(Point p, List<Point> points) {
+        System.out.println(p.toString());
+        for (Point point : points) {
+            System.out.print(point.toString() + " ");
+        }
+        System.out.println("");;
     }
 }
