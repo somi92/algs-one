eae665d4ccc6264fc4a688b9dabcede069b777bb
Milos Stojanovic <stojanovicmilos92@outlook.com>
2016-11-03 21:01:20 +0100

    Setting up kd trees task

---
+++ src/tasks/PointSET.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tasks;
+
+import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.RectHV;
+
+/**
+ *
+ * @author milos
+ */
+public class PointSET {
+
+    public PointSET() {
+
+    }
+
+    public boolean isEmpty() {
+
+        return false;
+    }
+
+    public int size() {
+
+        return 0;
+    }
+
+    public void insert(Point2D p) {
+
+    }
+
+    public boolean contains(Point2D p) {
+
+        return false;
+    }
+
+    public void draw() {
+
+    }
+
+    public Iterable<Point2D> range(RectHV rect) {
+
+        return null;
+    }
+
+    public Point2D nearest(Point2D p) {
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
