e50a51b7a61365cd0593c01673cf58e8b9b51aa7
Milos Stojanovic <stojanovicmilos92@outlook.com>
2016-11-08 21:38:47 +0100

    Implementing kd tree

--- src/tasks/KdTree.java
+++ src/tasks/KdTree.java
@@ -14,26 +14,107 @@ import edu.princeton.cs.algs4.RectHV;
  */
 public class KdTree {
 
-    public KdTree() {
+    private static final int X_COOR = 1;
+    private static final int Y_COOR = 2;
 
+    private Node root;
+    private int size;
+
+    public KdTree() {
+        root = null;
     }
 
     public boolean isEmpty() {
-
-        return false;
+        return root == null;
     }
 
     public int size() {
-
-        return 0;
+        return size;
     }
 
     public void insert(Point2D p) {
+        Node currentNode = root;
+        Node newNode = new Node(p);
+        
+        while (currentNode != null) {
+            if (currentNode.point.equals(newNode.point)) {
+                return;
+            }
 
+            if (currentNode.type == X_COOR) {
+                
+                if (currentNode.point.x() < newNode.point.x()) {
+                    // go to the left
+                    if(currentNode.left == null) {
+                        // add new node to the left, increment size
+                        newNode.type = Y_COOR;
+                        currentNode.left = newNode;
+                        size++;
+                        return;
+                    }
+                    currentNode = currentNode.left;
+                } else {
+                    // go to the right
+                    if(currentNode.right == null) {
+                        // add new node to the right, increment size
+                        newNode.type = Y_COOR;
+                        currentNode.right = newNode;
+                        size++;
+                        return;
+                    }
+                    currentNode = currentNode.right;
+                }
+            }
+
+            if (currentNode.type == Y_COOR) {
+                
+                if (currentNode.point.y() < newNode.point.y()) {
+                    // go to the left
+                    if(currentNode.left == null) {
+                        // add new node to the left, increment size
+                        newNode.type = X_COOR;
+                        currentNode.left = newNode;
+                        size++;
+                        return;
+                    }
+                    currentNode = currentNode.left;
+                } else {
+                    // go to the right
+                    if(currentNode.right == null) {
+                        // add new node to the right, increment size
+                        newNode.type = X_COOR;
+                        currentNode.right = newNode;
+                        size++;
+                        return;
+                    }
+                    currentNode = currentNode.right;
+                }
+            }
+        }
+        
+        currentNode = newNode;
+        size++;
     }
 
     public boolean contains(Point2D p) {
+        Node currentNode = root;
 
+        while (currentNode != null) {
+
+            if (currentNode.point.equals(p)) {
+                return true;
+            }
+
+            if (currentNode.type == X_COOR) {
+                currentNode = currentNode.point.x() < p.x()
+                        ? currentNode.left : currentNode.right;
+            }
+
+            if (currentNode.type == Y_COOR) {
+                currentNode = currentNode.point.y() < p.y()
+                        ? currentNode.left : currentNode.right;
+            }
+        }
         return false;
     }
 
@@ -49,6 +130,28 @@ public class KdTree {
     public Point2D nearest(Point2D p) {
 
         return null;
+    }
+
+    private static class Node {
+
+        private Point2D point;
+        private Node left;
+        private Node right;
+        private int type;
+
+        public Node(Point2D point) {
+            this.point = point;
+            left = null;
+            right = null;
+            type = X_COOR;
+        }
+
+        public Node(Point2D point, Node left, Node right, int type) {
+            this.point = point;
+            this.left = left;
+            this.right = right;
+            this.type = type;
+        }
     }
 
 //    public static void main(String[] args) {
